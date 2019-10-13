e5faf417160848553d551b8fc301b1996f76476c
Arpik <arpiksargsyan@gmail.com>
2019-10-13 17:48:31 +0400

    Added thumbnail and profile_picture attributes to Post and Author class.

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,13 +5,15 @@ User = get_user_model
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
+    profile_picture = models.ImageField()
+    
 class Post(models.Model):
     title = models.CharField(max_length = 100)
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     comment_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = models.ImageField()
 
     def __str__(self):
         return self.title
