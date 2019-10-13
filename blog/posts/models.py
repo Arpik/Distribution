b966fe630566ae8ff633063f5cfc7a2748c5838e
Arpik <arpiksargsyan@gmail.com>
2019-10-13 18:18:38 +0400

    Fixed User by adding missing ().

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
