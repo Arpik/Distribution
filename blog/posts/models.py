2806ba0f81a063595179c78ab07c730757bb02b5
Arpik <arpiksargsyan@gmail.com>
2019-10-13 17:39:41 +0400

    Added Post model to the models.py file

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    comment_count = models.IntegerField(default=0)
+    author = models.ForeignKey()
+    
+
