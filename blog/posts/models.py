fb11457e9d858ba7fdd95f125ca76f2645969ce0
Arpik <arpiksargsyan@gmail.com>
2019-10-13 17:51:19 +0400

    Added Category class to models.py file.

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -7,6 +7,12 @@ class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField()
     
+class Category(models.Model):
+    title = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
     title = models.CharField(max_length = 100)
     overview = models.TextField()
