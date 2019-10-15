4191101b826c94381f701eeef04d4e04abe76c27
Arpik <arpiksargsyan@gmail.com>
2019-10-15 14:20:05 +0400

    Added if statement to index view to get the email from the request.

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Post
+from marketion.models import Signup
 
 def index(request):
     featured = Post.objects.filter(featured = True)
     latest = Post.objects.order_by('-timestamp')[0:3]
+    
+    if request.method == "POST":
+        email = request.POST["email"]
+
     context = {
         'object_list': featured,
         'latest':latest 
